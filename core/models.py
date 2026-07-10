4aa93be764cc513b74d772f67f505fad4fa285e3
aban <abankhalili82@gnamil.com>
2026-07-10 15:27:09 +0330

    add verbose name

--- core/models.py
+++ core/models.py
@@ -4,30 +4,38 @@ from django.db import models
 
 
 class CountryChoices(models.TextChoices):
-    IRAN = ('iran','ایران')
-    FRANCE=('france', 'فرانسه')
+    IRAN = ('iran', 'ایران')
+    FRANCE = ('france', 'فرانسه')
 
 
 class User(models.Model):
     username = models.CharField(max_length=50, verbose_name='نام کاربری')
-    password = models.CharField(max_length=20)
-    name = models.CharField(max_length=50)
-    birthdate = models.DateField(null=True)
-    email = models.EmailField()
-    coutry = models.CharField(max_length=20, choices=CountryChoices.choices, default=CountryChoices.IRAN)
-    phone = models.CharField(max_length=11)
-    height = models.PositiveSmallIntegerField(null=True)
-    income = models.DecimalField(max_digits=8, decimal_places=2)
+    password = models.CharField(max_length=20, verbose_name='رمز عبور')
+    name = models.CharField(max_length=50, verbose_name='نام')
+    birthdate = models.DateField(null=True, verbose_name='تاریخ تولد')
+    email = models.EmailField(verbose_name='آدرس ایمیل')
+    coutry = models.CharField(
+        max_length=20, choices=CountryChoices.choices, default=CountryChoices.IRAN, verbose_name='کشور')
+    phone = models.CharField(max_length=11, verbose_name='شماره تماس')
+    height = models.PositiveSmallIntegerField(null=True, verbose_name='قد')
+    income = models.DecimalField(
+        max_digits=8, decimal_places=2, verbose_name='درآمد')
 
     class Meta:
         verbose_name = 'کاربر'
         verbose_name_plural = 'کاربران'
 
+
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_upate = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255, verbose_name='موضوع')
+    content = models.TextField(verbose_name='محتوا')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='ساخته شده توسط:')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='ساخته شده در:')
+    last_upate = models.DateTimeField(
+        auto_now=True, verbose_name='آخرین تغییرات')
 
-
+    class Meta:
+        verbose_name = 'پست'
+        verbose_name_plural = 'پست ها'
